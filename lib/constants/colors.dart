cd81720868df9355e97690c8f89fc7eb9b498c01
Shahzaib Ahmad <shahzaibahmadfd01@gmail.com>
2024-06-11 21:37:10 +0500

    Defining Colors.

---
+++ lib/constants/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+const Color bgClr = Colors.white;
+const Color blueClr = Color(0xff08408B);
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:GlobalTalk/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xff08408B),
+      color: bgClr,
       child: const Text('Home'),
     );
   }
